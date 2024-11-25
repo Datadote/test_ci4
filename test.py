1d9f6e80c29037fa8b804848ffc7b16fc188ab93
Datadote <datadote@gmail.com>
2024-11-24 23:52:36 -0800

    make all variables used

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import numpy as np
 def main():
     """ Returns 1 + 1 """
     df = pd.DataFrame({'a': [1, 2]})
-    test = np.array([1, 2])
-    return 1 + 1
+    test = np.array(df)
+    return test
 
 if __name__ == '__main__':
     main()
