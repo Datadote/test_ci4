89fcc5a4c5508b6a111a0693a7469aec1433a000
Datadote <datadote@gmail.com>
2024-11-30 21:55:11 -0800

    black and isort changes

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 """ Module provides 1 + 1 """
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 def main():
     """Returns 1 + 1."""
-    df = pd.DataFrame({'a': [1, 2]})
+    df = pd.DataFrame({"a": [1, 2]})
     test = np.array(df)
     return test
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
